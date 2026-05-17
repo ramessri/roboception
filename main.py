db234c7fbd1b2a3d536b92b3acfd32419cd391d4
Venkatsnv01 <venkatsnv01@gmail.com>
2026-05-17 16:15:27 +0200

    setup ros2 dockerfiles and models structure

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+import time
+
+url = "http://192.168.0.102:8080/video"
+
+cap = cv2.VideoCapture(url)
+
+fps_time = time.time()
+frames = 0
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("frame failed")
+        break
+
+    frames += 1
+
+    if time.time() - fps_time >= 1:
+        print("FPS:", frames)
+        frames = 0
+        fps_time = time.time()
+
+    cv2.imshow("cam", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
